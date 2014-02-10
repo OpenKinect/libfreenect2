662f50abfd9b88dc726f6a45655ab365a308ef5a
Christian Kerl <christian.kerl@in.tum.de>
2014-02-10 16:22:53 +0100

    added patch to increase libusbx MAX_ISO_BUFFER_LENGTH

--- depends/install_ubuntu.sh
+++ depends/install_ubuntu.sh
@@ -11,6 +11,7 @@ rm -rf $LIBUSBX_SOURCE_DIR $LIBUSBX_INSTALL_DIR
 git clone -b superspeed https://github.com/JoshBlake/libusbx.git $LIBUSBX_SOURCE_DIR
 
 cd $LIBUSBX_SOURCE_DIR
+git apply $DEPENDS_DIR/linux_usbfs_increase_max_iso_buffer_length.patch
 ./bootstrap.sh
 ./configure --prefix=$LIBUSBX_INSTALL_DIR
 make && make install
