8be7c480d6e6b328319eae3bc1ea868e60173d65
Christian Kerl <christian.kerl@in.tum.de>
2014-02-11 18:34:19 +0100

    increased opengl version to 3.3

--- examples/protonect/Protonect.cpp
+++ examples/protonect/Protonect.cpp
@@ -769,7 +769,7 @@ int main(int argc, char *argv[])
 
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
