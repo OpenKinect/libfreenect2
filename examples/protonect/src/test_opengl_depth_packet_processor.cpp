fe196ff3b49f8fcbcc40fd8acc48d141df70cdc3
Gabor Papp <gabor.papp@gmail.com>
2014-12-08 20:10:07 +0100

    test_opengl OSX fix

--- examples/protonect/src/test_opengl_depth_packet_processor.cpp
+++ examples/protonect/src/test_opengl_depth_packet_processor.cpp
@@ -58,6 +58,9 @@ int main(int argc, char **argv) {
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
