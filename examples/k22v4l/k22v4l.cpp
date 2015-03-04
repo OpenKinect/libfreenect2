4aca4f88d011c018b9584690deed853604c07127
jfrey <nomhad@gmail.com>
2015-03-04 20:48:44 +0100

    Working v4l feed for RGB (!).

--- examples/k22v4l/k22v4l.cpp
+++ examples/k22v4l/k22v4l.cpp
@@ -34,6 +34,14 @@
 #include <libfreenect2/frame_listener_impl.h>
 #include <libfreenect2/threading.h>
 
+#include <linux/videodev2.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+
+#define VIDEO_DEVICE_RGB "/dev/video0"
+#define VIDEO_DEVICE_DEPTH "/dev/video1"
+#define VIDEO_DEVICE_IR "/dev/video2"
+
 bool k22v4l_shutdown = false;
 
 void sigint_handler(int s)
@@ -75,18 +83,54 @@ int main(int argc, char *argv[])
 
   std::cout << "device serial: " << dev->getSerialNumber() << std::endl;
   std::cout << "device firmware: " << dev->getFirmwareVersion() << std::endl;
+  
+  // init V4L loopback output
+  int fd_rgb = open(VIDEO_DEVICE_RGB, O_RDWR);
+  assert(fd_rgb >= 0);
+  // what should handle
+  struct v4l2_capability vid_caps;
+  int ret_code = ioctl(fd_rgb, VIDIOC_QUERYCAP, &vid_caps);
+  assert(ret_code != -1);
+  
+  std::cout << "init feed" << std::endl;
+  // first run to get pointer to frame
+  listener.waitForNewFrame(frames);
+  libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
+  libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
+  libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
+    
+  std::cout << "configure v4l loopback for RGB" << std::endl;
+  // configure for our RBG device
+  struct v4l2_format vid_format_rgb;
+  vid_format_rgb.type = V4L2_BUF_TYPE_VIDEO_OUTPUT;
+  vid_format_rgb.fmt.pix.width = rgb->width;
+  vid_format_rgb.fmt.pix.height = rgb->height;
+  vid_format_rgb.fmt.pix.pixelformat = V4L2_PIX_FMT_BGR24;
+  vid_format_rgb.fmt.pix.sizeimage = rgb->width * rgb->height * 3;
+  vid_format_rgb.fmt.pix.field = V4L2_FIELD_NONE;
+  vid_format_rgb.fmt.pix.bytesperline = rgb->width * 3;
+  vid_format_rgb.fmt.pix.colorspace = V4L2_COLORSPACE_SRGB;
+  ret_code = ioctl(fd_rgb, VIDIOC_S_FMT, &vid_format_rgb);
+  assert(ret_code != -1);
 
+  std::cout << "start loop" << std::endl;
+  
   while(!k22v4l_shutdown)
   {
+
     listener.waitForNewFrame(frames);
-    libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
-    libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
-    libfreenect2::Frame *depth = frames[libfreenect2::Frame::Depth];
+    rgb = frames[libfreenect2::Frame::Color];
+    ir = frames[libfreenect2::Frame::Ir];
+    depth = frames[libfreenect2::Frame::Depth];
 
-    cv::imshow("rgb", cv::Mat(rgb->height, rgb->width, CV_8UC3, rgb->data));
-    cv::imshow("ir", cv::Mat(ir->height, ir->width, CV_32FC1, ir->data) / 20000.0f);
-    cv::imshow("depth", cv::Mat(depth->height, depth->width, CV_32FC1, depth->data) / 4500.0f);
+    //cv::imshow("rgb", cv::Mat(rgb->height, rgb->width, CV_8UC3, rgb->data));
+    //cv::imshow("ir", cv::Mat(ir->height, ir->width, CV_32FC1, ir->data) / 20000.0f);
+    //cv::imshow("depth", cv::Mat(depth->height, depth->width, CV_32FC1, depth->data) / 4500.0f);
 
+    ret_code = write(fd_rgb, rgb->data, rgb->width * rgb->height * 3);
+    
+    std::cout << "written: " << ret_code << std::endl;
+    
     int key = cv::waitKey(1);
     k22v4l_shutdown = k22v4l_shutdown || (key > 0 && ((key & 0xFF) == 27)); // shutdown on escape
 
@@ -99,5 +143,7 @@ int main(int argc, char *argv[])
   dev->stop();
   dev->close();
 
+  close(fd_rgb);
+    
   return 0;
 }
