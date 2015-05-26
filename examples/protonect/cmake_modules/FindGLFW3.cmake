8ba8e2226c5c819f4ba26b71414cc42e5bfe49a2
Lars Glud <larshg@gmail.com>
2015-05-26 11:09:16 +0200

    Corrected indention.
    Removed Lib found announcement.
    Removed lib was already known.

--- examples/protonect/cmake_modules/FindGLFW3.cmake
+++ examples/protonect/cmake_modules/FindGLFW3.cmake
@@ -8,10 +8,9 @@
 include(FindPackageHandleStandardArgs)
 
 IF(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
-	find_path(GLFW3_INCLUDE_DIRS glfw/glfw3.h DOC "GLFW include directory " HINTS $ENV{GLFW_ROOT}/include)
-	
-	find_library(GLFW3_LIBRARIES NAMES glfw3dll.lib HINTS $ENV{GLFW_ROOT}/lib/)
-	
+  find_path(GLFW3_INCLUDE_DIRS glfw/glfw3.h DOC "GLFW include directory " HINTS $ENV{GLFW_ROOT}/include)
+  
+  find_library(GLFW3_LIBRARIES NAMES glfw3dll.lib HINTS $ENV{GLFW_ROOT}/lib/)
 ENDIF()
 
 find_package_handle_standard_args(GLFW3 "Could not find GLFW3 - try adding GLFW_ROOT in enviroment variables." GLFW3_INCLUDE_DIRS GLFW3_LIBRARIES)
