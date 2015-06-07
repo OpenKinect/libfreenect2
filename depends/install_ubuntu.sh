fe0980746a898b52d5104e358903c5618274f63d
Florian Echtler <floe@butterbrot.org>
2015-06-07 17:46:21 +0200

    split depends files, install glfw from Ubuntu packages

--- depends/install_ubuntu.sh
+++ depends/install_ubuntu.sh
@@ -3,11 +3,15 @@
 cd `dirname $0`
 DEPENDS_DIR=`pwd`
 
+ARCH="$(uname -m | grep -q 64 && echo 'amd64' || echo 'i386')"
+
 # download standalone packages for 14.04 LTS
-wget http://launchpadlibrarian.net/173940430/libglfw3_3.0.4-1_amd64.deb
-wget http://launchpadlibrarian.net/173940431/libglfw3-dev_3.0.4-1_amd64.deb
-wget http://launchpadlibrarian.net/173940397/libglfw3-doc_3.0.4-1_all.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/glfw3/libglfw3_3.0.4-1_${ARCH}.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/glfw3/libglfw3-dev_3.0.4-1_${ARCH}.deb
+wget http://mirrors.kernel.org/ubuntu/pool/universe/g/glfw3/libglfw3-doc_3.0.4-1_all.deb
 
-sudo dpkg -i libglfw3*_3.0.4-1_amd64.deb
+sudo dpkg -i libglfw3*_3.0.4-1_*.deb
 
-sh ./install_deps.sh
+sudo apt-get install -y build-essential libjpeg-turbo8-dev libtool autoconf libudev-dev cmake mesa-common-dev freeglut3-dev libxrandr-dev doxygen libxi-dev libopencv-dev automake
+
+sh ./install_libusb.sh
