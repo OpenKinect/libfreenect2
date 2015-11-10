40ba27cfce13c7cea0491e7b1e963e5cf609933b
Florian Echtler <floe@butterbrot.org>
2015-11-10 15:01:46 +0100

    checkout libusb 1.0.20 for manual install, closes #466

--- depends/install_libusb.sh
+++ depends/install_libusb.sh
@@ -13,7 +13,7 @@ rm -rf $LIBUSB_SOURCE_DIR $LIBUSB_INSTALL_DIR
 git clone https://github.com/libusb/libusb.git $LIBUSB_SOURCE_DIR
 
 cd $LIBUSB_SOURCE_DIR
-git checkout 51b10191033ca3a3819dcf46e1da2465b99497c2
+git checkout v1.0.20
 ./bootstrap.sh
 ./configure --prefix=$LIBUSB_INSTALL_DIR
 make && make install
