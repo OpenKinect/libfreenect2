896108d91e583d57f91f36c886f62b25a7945f84
Brendan Burns <bburns@google.com>
2016-02-02 20:19:48 -0800

    Add accessors for the various depth tables.

--- include/libfreenect2/packet_pipeline.h
+++ include/libfreenect2/packet_pipeline.h
@@ -31,6 +31,8 @@
 
 #include <libfreenect2/config.h>
 
+#include <stdlib.h>
+
 namespace libfreenect2
 {
 
@@ -71,6 +73,13 @@ protected:
  public:
    DumpPacketPipeline();
    virtual ~DumpPacketPipeline();
+
+   // These are all required to decode depth data
+   const unsigned char* getDepthP0Tables(size_t* length);
+
+   const float* getDepthXTable(size_t* length);
+   const float* getDepthZTable(size_t* length);
+   const short* getDepthLookupTable(size_t* length);
  };
 
 /** Pipeline with CPU depth processing. */
