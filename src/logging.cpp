8efb59b242f02de568c95278bb55aa032a6c4d4f
Lingzhu Xiang <lingzhu@lingzhuxiang.com>
2016-02-18 10:36:49 -0500

    logging: Add an option to collect profiling
    Use cmake -DENABLE_PROFILING=ON (OFF by default).

--- src/logging.cpp
+++ src/logging.cpp
@@ -32,6 +32,13 @@
 #include <string>
 #include <algorithm>
 
+#ifdef LIBFREENECT2_WITH_PROFILING
+#include <vector>
+#include <numeric>
+#include <functional>
+#include <cmath>
+#endif
+
 #ifdef LIBFREENECT2_WITH_CXX11_SUPPORT
 #include <chrono>
 #endif
@@ -192,6 +199,9 @@ class Timer
 
   Timer()
   {
+#if defined(LIBFREENECT2_WITH_OPENGL_SUPPORT)
+    glfwInit();
+#endif
     reset();
   }
 
@@ -209,10 +219,13 @@ class Timer
     time_start = std::chrono::high_resolution_clock::now();
   }
 
-  void stop()
+  double stop()
   {
-    duration += std::chrono::duration_cast<std::chrono::duration<double>>(std::chrono::high_resolution_clock::now() - time_start).count();
+    auto time_diff = std::chrono::high_resolution_clock::now() - time_start;
+    double this_duration = std::chrono::duration_cast<std::chrono::duration<double>>(time_diff).count();
+    duration += this_duration;
     count++;
+    return this_duration;
   }
 #elif defined(LIBFREENECT2_WITH_OPENGL_SUPPORT)
   double time_start;
@@ -222,18 +235,21 @@ class Timer
     time_start = glfwGetTime();
   }
 
-  void stop()
+  double stop()
   {
-    duration += glfwGetTime() - time_start;
+    double this_duration = glfwGetTime() - time_start;
+    duration += this_duration;
     count++;
+    return this_duration;
   }
 #else
   void start()
   {
   }
 
-  void stop()
+  double stop()
   {
+    return 0;
   }
 #endif
 };
@@ -241,9 +257,46 @@ class Timer
 class WithPerfLoggingImpl: public Timer
 {
 public:
+#ifdef LIBFREENECT2_WITH_PROFILING
+  std::vector<double> stats;
+  std::string name;
+
+  WithPerfLoggingImpl()
+  {
+    stats.reserve(30*100);
+  }
+
+  ~WithPerfLoggingImpl()
+  {
+    if (stats.size() < 2)
+      return;
+    std::vector<double> &v = stats;
+    std::sort(v.begin(), v.end());
+    double sum = std::accumulate(v.begin(), v.end(), 0.0);
+    size_t n = v.size();
+    double mean = sum / n;
+    std::vector<double> diff(n);
+    std::transform(v.begin(), v.end(), diff.begin(), std::bind2nd(std::minus<double>(), mean));
+    double sqsum = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
+    double std = std::sqrt(sqsum / (n-1));
+
+    std::cout << name << v[0] << " " << v[n/20] << " " << v[n/2] << " " << v[n - (n+19)/20] << " " << v[n-1] << " mean=" << mean << " std=" << std <<  " n=" << n << std::endl;
+  }
+#endif
+
   std::ostream &stop(std::ostream &stream)
   {
+#ifndef LIBFREENECT2_WITH_PROFILING
     Timer::stop();
+#else
+    double this_duration = Timer::stop();
+    if (name.empty())
+    {
+      std::stringstream &ss = static_cast<std::stringstream &>(stream);
+      name = ss.str();
+    }
+    stats.push_back(this_duration*1e3);
+#endif
     if (count < 100)
     {
       stream.setstate(std::ios::eofbit);
