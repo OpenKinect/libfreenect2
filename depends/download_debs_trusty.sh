619bd52f9044dd704c5885e16c5b8d7998972e14
Lingzhu Xiang <lingzhu@lingzhuxiang.com>
2016-03-23 19:26:53 +0000

    depends: Do not download libva debs for non-x86

--- depends/download_debs_trusty.sh
+++ depends/download_debs_trusty.sh
@@ -25,5 +25,7 @@ download() {
 download main/libu/libusb-1.0 1.0.20-1 libusb-1.0-0-dev libusb-1.0-0
 download universe/g/glfw3 3.1.2-3 libglfw3-dev libglfw3
 download main/o/ocl-icd 2.2.8-1 ocl-icd-libopencl1 ocl-icd-opencl-dev
-download universe/libv/libva 1.7.0-1 libva-dev libva-drm1 libva-egl1 libva-glx1 libva-tpi1 libva-wayland1 libva-x11-1 libva1 vainfo
-download universe/i/intel-vaapi-driver 1.7.0-1 i965-va-driver
+if [ "$ARCH" = amd64 -o "$ARCH" = i386 ]; then
+  download universe/libv/libva 1.7.0-1 libva-dev libva-drm1 libva-egl1 libva-glx1 libva-tpi1 libva-wayland1 libva-x11-1 libva1 vainfo
+  download universe/i/intel-vaapi-driver 1.7.0-1 i965-va-driver
+fi
