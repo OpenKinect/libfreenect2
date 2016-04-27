83f88b4c09f0b00724ae65785abcd4f3eeb79f93
Lingzhu Xiang <lingzhu@lingzhuxiang.com>
2016-04-27 16:34:51 -0400

    Update author list for 0.2 release

--- depends/make_release_msvc.cmd
+++ depends/make_release_msvc.cmd
@@ -27,6 +27,7 @@ rmdir /s /q install\lib\cmake
 rmdir /s /q install\lib\pkgconfig
 copy ..\LICENSES.txt install
 copy ..\INSTALL-windows.txt install
+copy ..\..\CONTRIB install
 cd ..
 rmdir /s /q libfreenect2-%ver%-vs%vs_year%-x64
 move build\install libfreenect2-%ver%-vs%vs_year%-x64
